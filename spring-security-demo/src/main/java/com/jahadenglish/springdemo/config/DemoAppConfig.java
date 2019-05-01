34026299f8331a0891a62dca2af68de4821b7818
jahad <jahad@DESKTOP-OO4A538.home>
2019-05-01 09:08:56 -0400

    first test to make sure code works with server

---
+++ spring-security-demo/src/main/java/com/jahadenglish/springdemo/config/DemoAppConfig.java
@@ -0,0 +1,29 @@
+package com.jahadenglish.springdemo.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.ViewResolver;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.view.InternalResourceViewResolver;
+
+@Configuration
+@EnableWebMvc
+@ComponentScan(basePackages="com.jahadenglish.springdemo")
+public class DemoAppConfig {
+
+	//define a bean for the view resolver
+	
+	@Bean
+	public ViewResolver viewResolver() {
+		
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+		
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+		
+		return viewResolver;
+	}
+	
+	
+}
---
+++ spring-security-demo/src/main/webapp/WEB-INF/view/home.jsp
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html>
+<head>
+
+	<title> Jahad Home page</title>
+</head>
+
+<body>
+
+<h1> Jahad Page </h1>
+<hr>
+
+</body>
+
+
+
+
+</html>
